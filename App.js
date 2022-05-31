7752eaaf75e85b385facc229aa954e50c6ca3a1e
sonoasy <61787171+sonoasy@users.noreply.github.com>
2022-05-31 23:38:40 +0900

    문장 출력

--- App.js
+++ App.js
@@ -5,7 +5,7 @@
  * @format
  * @flow strict-local
  */
-
+/*
 import React from 'react';
 import type {Node} from 'react';
 import {
@@ -106,6 +106,35 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+});
+
+export default App;
+*/
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.appTitle}>Hello Todolist</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#3143e8',
+  },
+  appTitle: {
+    color: '#fff',
+    fontSize: 36,
+    marginTop: 30,
+    marginBottom: 30,
+    fontWeight: '300',
+    textAlign: 'center',
+    backgroundColor: '#3143e8',
   },
 });
 
