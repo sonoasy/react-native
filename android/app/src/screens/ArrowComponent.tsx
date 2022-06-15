1110e9d8402730bfc4ba8ea366227a40f4d09ff2
sonoasy <61787171+sonoasy@users.noreply.github.com>
2022-06-15 21:22:06 +0900

    button

---
+++ android/app/src/screens/ArrowComponent.tsx
@@ -0,0 +1,14 @@
+// ./src/screens/ArrowComponent.tsx
+/*import React from 'react';
+import {Text} from 'react-native';
+
+//const ArrowComponent =()=> //속성 값이 있을때 
+
+
+function ArrowComponent() {
+  return (
+    <Text>함수 컴포넌트</Text>
+  )
+}
+
+export default ArrowComponent*/
